b1642b535c0de3ca1ad1a6c080d2411f0dc1fe6d
Ryan Smith <ryan.l.smith4@gmail.com>
2019-04-24 09:49:05 -0700

    script refinement

--- sentry-install.sh
+++ sentry-install.sh
@@ -3,6 +3,7 @@
 # Generate a random secret key and put it into the environment variable file
 # run this after setting up sentry server
 docker-compose run --rm sentry-base sentry config generate-secret-key
+# echo -e "\nSENTRY_SECRET_KEY=$(docker-compose run --rm sentry-base sentry config generate-secret-key)"
 
 # Run database migrations (build the database)
 docker-compose run --rm sentry-base sentry upgrade --noinput
